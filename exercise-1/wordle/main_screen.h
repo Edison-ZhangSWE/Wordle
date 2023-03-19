1201207e320e0a0c77da63ed9c82391ddb17c8c2
Edison Zhang <ezhang33@ucmerced.edu>
2023-03-19 22:45:18 +0000

    Game/Main Screen

--- exercise-1/wordle/main_screen.h
+++ exercise-1/wordle/main_screen.h
@@ -8,50 +8,51 @@
 #include <iostream>
 #include <vector>
 
-void printTopScreen(string COLOR) {
-    cout << COLOR << " --- " << RESET;
+void printTopScreen(std::string COLOR) {
+    std::cout << COLOR << " --- " << RESET;
     }
-void printMiddleScreen(char a, string COLOR) {
-    cout << COLOR << "| " << a << " |" << RESET;
+void printMiddleScreen(char a, std::string COLOR) {
+    std::cout << COLOR << "| " << a << " |" << RESET;
     }
-void printBottomScreen(string COLOR) {
-    cout << COLOR << " --- " << RESET;
+void printBottomScreen(std::string COLOR) {
+    std::cout << COLOR << " --- " << RESET;
     }
 void printScreen(std::vector<std::string> guesses, std::string correctWord, std::string wordleGameState, std::vector<std::vector<std::string>> colors){
-
+    system("clear");
+    
 for (int i = 0; i<5; i++){ // loops through our guesses
     for (int j = 0; j<5; j++){
         printTopScreen(colors[i][j]); // prints the top of the header with our specified color
 }
-std::cout << endl;
+std::cout << std::endl;
 for (int i = 0; i<5; i++){
     printMiddleScreen(guesses[i][j], colors[i][j]); // prints the middle part, with the array index of our guess
     // array of an array because there are multiple guesses and multiple letters in a guess. same for colors
 }
-std::cout << endl; 
+std::cout << std::endl; 
 for (int i = 0; i<5; i++){
     printBottomScreen(colors[i][j]);
 }
-std::cout << endl;
+std::cout << std::endl;
 }
-std::cout << endl;
-std::cout << endl;
-std::cout << endl;
+std::cout << std::endl;
+std::cout << std::endl;
+std::cout << std::endl;
 // VICTORY SCREEN
 if (wordleGameState == "win"){
-cout << endl;
-cout << "Splendid!" << endl;
-cout << endl;
-cout << "Press [enter] to continue";
+std::cout << std::endl;
+std::cout << "Splendid!" << std::endl;
+std::cout << std::endl;
+std::cout << "Press [enter] to continue";
 }
 // DEFEAT SCREEN
 else if (wordleGameState == "lose"){
-cout << endl;
-cout << "The word was: " << correctword << endl;
-cout << endl;
-cout << "Better luck next time!" << endl;
-cout << endl;
-cout << "Press [enter] to continue";
+std::cout << std::endl;
+std::cout << "The word was: " << correctWord << std::endl;
+std::cout << std::endl;
+std::cout << "Better luck next time!" << std::endl;
+std::cout << std::endl;
+std::cout << "Press [enter] to continue";
 }
 }
 #endif
